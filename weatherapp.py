3a60fb3b2f4163f05534c543052923915efe9209
sajiyyafathiman180608-hub <sajiyyafathiman180608@gmail.com>
2026-03-13 21:14:16 +0530

    Add files via upload

---
+++ weatherapp.py
@@ -0,0 +1,22 @@
+import requests
+print("------ WEATHER APP ------")
+api_key = "dac075e14cdaf79bc98b897e873339bc"
+city = input("\nEnter city name: ")
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+response = requests.get(url)
+data = response.json()
+if data["cod"] == 200:
+    city_name = data["name"]
+    country = data["sys"]["country"]
+    temp = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    weather = data["weather"][0]["description"]
+    wind = data["wind"]["speed"]
+    print("\n------ WEATHER REPORT ------\n")
+    print("City:", city_name, ",", country)
+    print("Temperature:", temp, "°C")
+    print("Humidity:", humidity, "%")
+    print("Condition:", weather,"\n")
+else:
+    print("City not found")
+
